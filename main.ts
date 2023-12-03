a7a7972688d5ceab14f229e71fa24517c84e24e0
Theo Sun <theo.sun.github@outlook.com>
2023-12-03 17:08:19 +0800

    chore: update

--- main.ts
+++ main.ts
@@ -1,26 +1,11 @@
-import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import data from "./data.json" assert { type: "json" };
+import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
 const router = new Router();
 router
   .get("/", (context) => {
-    context.response.body = "Welcome to dinosaur API!";
-  })
-  .get("/api", (context) => {
-    context.response.body = data;
-  })
-  .get("/api/:dinosaur", (context) => {
-    if (context?.params?.dinosaur) {
-      const found = data.find((item) =>
-        item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
-      );
-      if (found) {
-        context.response.body = found;
-      } else {
-        context.response.body = "No dinosaurs found.";
-      }
-    }
+    context.response.type = "application/json";
+    context.response.body = { status: 200, name: "Theo Personal Exmaple APIs" };
   });
 
 const app = new Application();
